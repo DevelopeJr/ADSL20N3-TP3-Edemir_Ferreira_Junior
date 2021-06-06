fb8050e2c22e16f8c3120db17205c6abf3d5c616
DevelopeJr <edemir.ferreira.jr@gmail.com>
2021-06-06 12:45:18 -0300

    [ ADSL20N3-TP3-Edemir_Ferreira_Junior ]

---
+++ Data/Repositories/ProdutosRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Models;
+
+namespace Data.Repositories
+{
+    public class ProdutoRepository
+    {
+        private static List<ProdutoModel> _produtos = new()
+        {
+            new ProdutoModel
+            {
+                IdProduto = 1,
+                NomeProduto = "iPod Classic",
+                CaracteristicasProduto = "Silver - 160gb",
+                QuantidadeProduto = 2,
+                DataFabricacao = new DateTime(2009, 10, 01)
+
+            }
+
+
+        };
+        public IEnumerable<ProdutoModel> GetAll(string busca = null)
+        {
+            if (string.IsNullOrWhiteSpace(busca))
+            {
+                return _produtos;
+            }
+
+            return _produtos.Where(x => x.NomeProduto.Contains(busca));
+        }
+        public void Addition(ProdutoModel produtoModel)
+        {
+            if (_produtos.Count() == 0)
+            {
+                produtoModel.IdProduto = 0;
+            }
+            else
+            {
+                produtoModel.IdProduto = _produtos.LastOrDefault().IdProduto + 1;
+            }
+
+            _produtos.Add(produtoModel);
+        }
+        public ProdutoModel GetById(int id)
+        {
+            return _produtos.FirstOrDefault(x => x.IdProduto == id);
+        }
+
+        public void Update(ProdutoModel produtoModel)
+        {
+            var produto = GetById(produtoModel.IdProduto);
+
+            if (produto == null)
+            {
+                return;
+            }
+
+            produto.IdProduto = produtoModel.IdProduto;
+            produto.NomeProduto = produtoModel.NomeProduto;
+            produto.CaracteristicasProduto = produtoModel.CaracteristicasProduto;
+            produto.QuantidadeProduto = produtoModel.QuantidadeProduto;
+            produto.DataFabricacao = produtoModel.DataFabricacao;
+        }
+        public void Erase(int id)
+        {
+            var produto = GetById(id);
+
+            _produtos.Remove(produto);
+        }
+    }
+}
---
+++ Mvc/Controllers/ProdutoController.cs
@@ -0,0 +1,118 @@
+﻿using Data.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data.Models;
+
+namespace Mvc.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ProdutoRepository _produtoRepository;
+        public ProdutoController()
+        {
+            _produtoRepository = new ProdutoRepository();
+        }
+        // GET: ProdutoController
+         
+        public ActionResult Index(string busca = null)
+        {
+            var produtos = _produtoRepository.GetAll(busca);
+
+            ViewData["Busca"] = busca;
+
+            return View(produtos);
+        }
+
+        // GET: ProdutoController/Details/5
+        public ActionResult Details(int id)
+        {
+            var produto = _produtoRepository.GetById(id);
+
+            if (produto == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(produto);
+        }
+
+        // GET: ProdutoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProdutoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProdutoModel produtoModel)
+        {
+            try
+            {
+                _produtoRepository.Addition(produtoModel);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProdutoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var produto = _produtoRepository.GetById(id);
+
+            if (produto == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(produto);
+        }
+
+        // POST: ProdutoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ProdutoModel produtoModel)
+        {
+            try
+            {
+                _produtoRepository.Update(produtoModel);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(produtoModel);
+            }
+        }
+
+        // GET: ProdutoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var produto = _produtoRepository.GetById(id);
+
+            return View(produto);
+        }
+
+        // POST: ProdutoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Remove(int id)
+        {
+            try
+            {
+                _produtoRepository.Erase(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(nameof(Delete));
+            }
+        }
+    }
+}
